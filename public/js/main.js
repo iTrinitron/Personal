9b38e95493bdc53b7e4ac158246745644cac8651
Michael Chin <michaelchin1@Michaels-MacBook-Pro.local>
2015-08-07 16:31:13 -0700

    materialized

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+ 
+}]);
+
+angular.module('buttonsDemo1', ['ngMaterial'])
+.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+  $scope.googleUrl = 'http://google.com';
+});
